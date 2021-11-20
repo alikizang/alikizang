275ae1656bf05d0ba63dbf9e8f92b379b58793f8
Thershold <prosper.alikizang@gmail.com>
2021-11-20 10:13:00 +0000

    MyLeetcodeTraining

---
+++ Palindrome.py
@@ -0,0 +1,58 @@
+# def isPalindrome(x):
+#     # convert my int to string and force all element to lowercase
+#     s = str.lower(x)
+#     print(s)
+#     # create reversed string
+#     rs = "".join(list(reversed(s)))
+#     print(rs)
+#     # compare them
+#     if rs != s:
+#         return False
+#     return True
+#
+#
+# t = "taco cat"
+# ans = isPalindrome(t)
+# print(ans)
+
+
+# word = input('Enter any word: ')
+# rev = reversed(word)
+# if list(word) == list(rev):
+#     print('It is a palindrome')
+# else:
+#      print('It is not a palindrome')
+
+
+# w = input('Enter the word to see if it is a palindrome: ')
+# w = str.lower(w)
+# if w == w[::-1]:
+#     print("This is a palindrome")
+# else:
+#     print("This is not a palindrome")
+
+
+# # Take case of sentence
+# def isPalindrome(string):
+#     reversed_string = ''
+#     # Rem all the spaces
+#     s = string.replace(' ', '')
+#     # Making the whole string lowercase char
+#     s = s.lower()
+#     print(s)
+#     # create reversed string
+#     rs = "".join(list(reversed(s)))
+#     print(rs)
+#     # compare them
+#     if rs == s:
+#         print('Palindrome')
+#     else:
+#         print('Not a Palindrome')
+#
+#
+# isPalindrome("Too hot to hoot")
+
+
+
+
+
